dd7d2e66d528d36640a634a96da83d1cf240b28b
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-15 09:39:34 +0000

    Playing Around w SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    let count = 0
+
+    function increaseNumber(){
+        count +=1
+    }
+    function decreaseNumber(){
+        count -=1
+    }
+</script>
+
+
+<h1>Number : {count}</h1>
+
+<button on:click={increaseNumber}>Increment</button>
+<button on:click={decreaseNumber}>Decrement</button>
+<!-- Writing -->
+<a href="/about">Go To The About Page</a>
